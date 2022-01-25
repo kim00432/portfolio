000adfe21da154492dfc1909ee54f4e86404943e
kim00432 <kim00432@algonquinlive.com>
2022-01-24 19:26:15 -0500

    Update skill lists
    Update skill lists

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -16,39 +16,45 @@ export default function About() {
         </div>
         <div className="terminal-window">
             <div className="statement" id="top">
-                <p className="input">location &gt; 
-                <span className="return">&nbsp;ottawa, canada</span>
+                <p className="input">Location &gt; 
+                <span className="return">&nbsp;Ottawa, Canada</span>
                 </p>
             </div>
             <div className="statement">
-                <p className="input">education &gt; 
-                <span className="return">&nbsp;mobile application design &amp; development in algonquin college (Sep 2020 ~ present)</span>
+                <p className="input">Education &gt; 
+                <span className="return">&nbsp;Mobile application Design &amp; Development in algonquin college (Sep 2020 ~ present)</span>
                 </p>
             </div>
             <div className="statement">
-                <p className="input">contact &gt; 
+                <p className="input">Contact &gt; 
                 <span className="return"><a href="mailto:isyoonakim@gmail.com">isyoonakim@gmail.com</a></span>
                 </p>
             </div>
             <div className="statement">
-                <p className="input">resume &gt; 
-                <span className="return"><a href="https://drive.google.com/file/d/1RTEzArInvQYa_3GSBx7oY-gOq4-5GRFF/view?ths=true">yoonakim.pdf</a></span>
+                <p className="input">Resume &gt; 
+                <span className="return"><a href="https://drive.google.com/file/d/1RTEzArInvQYa_3GSBx7oY-gOq4-5GRFF/view?ths=true">Yoonakim.pdf</a></span>
                 </p>
             </div>
             <div className="statement">
-                <p className="input">skills &gt;</p>
-                <p className="return">front-end [ html, css, javascript, react/react native ]</p>
-                <p className="return">back-end [ node js, express, npm, rest, mongo db, docker, aws, firebase ]</p>
-                <p className="return">others [ github, vs code, adobe xd/photoshop/illustrator, figma ]</p>
+                <p className="input">Skills/<b>Programming</b> &gt;
+                <span className="return">&nbsp;[ HTML/CSS, JavaScript, React/React native, PWAs, Node.js, Express, Firebase, APIs ]</span></p>
             </div>
             <div className="statement">
-                <p className="input">languages &gt;
-                <span className="return">&nbsp;[ english, korean ]</span>
+                <p className="input">Skills/<b>Design</b> &gt;
+                <span className="return">&nbsp;[ User/Product Research, Personas&amp;Usage Scenarios, Information Architecture, Wireframing, Interactive Prototyping, Usability Testing ]</span></p>
+            </div>
+            <div className="statement">
+                <p className="input">Skills/<b>Tools</b> &gt;
+                <span className="return">&nbsp;[ Git/Github, VS Code, Adobe XD/Photoshop/Illustrator, Figma ]</span></p>
+            </div>
+            <div className="statement">
+                <p className="input">Languages &gt;
+                <span className="return">&nbsp;[ English, Korean ]</span>
                 </p>
             </div>
             <div className="statement">
-                <p className="input">interests &gt;
-                <span className="return">&nbsp;[ traveling, photography, lego, watching movies/tv shows, music, coffee ]</span>
+                <p className="input">Interests &gt;
+                <span className="return">&nbsp;[ Traveling, Photography, Lego, Watching movies/tv shows, Music, Coffee ]</span>
                 </p>
             </div>
             <div className="statement" id="bottom">
